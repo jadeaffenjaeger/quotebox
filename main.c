21ae2fc25bf5fb016d4de7109eece76e37fcc1e6
Elias <elias_trommer@gmx.de>
2019-04-21 15:02:07 +0200

    Enable and disable Player via external MOSFET

--- main.c
+++ main.c
@@ -5,20 +5,36 @@
 #include "gpio.h"
 
 
+void start_player();
+void stop_player();
+
 void main() {
+
+    /* Bring up hardware peripherals*/
     clock_init();
     dfplayer_init();
-    gpio_led_init();
-
-    dfplayer_reset();
-
-    awu_sleep(MSECONDS_100);
-    uint16_t test = dfplayer_get_tracks();
-    dfplayer_set_volume(20);
-    dfplayer_set_track(1);
+    gpio_init();
 
     for(;;) {
-        awu_sleep(SECONDS_1);
-        gpio_led_toggle();
+        gpio_led_on();
+        start_player();
+        dfplayer_set_track(1);
+        awu_sleep(SECONDS_10);
+        stop_player();
+        gpio_led_off();
+        awu_sleep(SECONDS_10);
     }
+}
+
+/* Do the necessary steps for the DFPlayer to be ready*/
+void start_player() {
+    gpio_fet_on();
+    dfplayer_reset();
+    dfplayer_wait_for_init();
+    dfplayer_set_volume(30);
+}
+
+/* Shut down DFPlayer*/
+void stop_player() {
+    gpio_fet_off();
 }
