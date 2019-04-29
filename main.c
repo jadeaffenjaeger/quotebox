21bc5a7ddb149b61971984dee8e606d3c1090940
Elias <elias_trommer@gmx.de>
2019-04-29 16:29:33 +0200

    Implement working version in main

--- main.c
+++ main.c
@@ -10,19 +10,46 @@ void stop_player();
 
 void main() {
 
+    /* Delays to go to sleep for*/
+    uint16_t sleep_times[] = {5, 6, 3, 2, 1, 5};
+    uint8_t sleep_idx = 0;
+    uint8_t track_idx = 1;
+
     /* Bring up hardware peripherals*/
     clock_init();
     dfplayer_init();
     gpio_init();
 
+    /* Get number of tracks on SD card to loop through*/
+    start_player();
+    uint16_t tracks = dfplayer_get_tracks();
+
     for(;;) {
+        /* Bring up player*/
         gpio_led_on();
         start_player();
-        dfplayer_set_track(1);
-        awu_sleep(SECONDS_10);
+
+        /* Play next track*/
+        dfplayer_set_track(track_idx++);
+        if (track_idx > tracks) {
+            track_idx = 1;
+        }
+
+        /* wait for Player to finish*/
+        do {
+            awu_sleep(MSECONDS_100);
+        } while(gpio_player_busy() == true);
+
+        /* shut down player*/
         stop_player();
         gpio_led_off();
-        awu_sleep(SECONDS_10);
+
+        /* Go to sleep for next interval*/
+        awu_sleep_n_seconds(sleep_times[sleep_idx++]);
+        /* FIXME make arr length variable*/
+        if (sleep_idx >= 6) {
+            sleep_idx = 0;
+        }
     }
 }
 
@@ -31,7 +58,7 @@ void start_player() {
     gpio_fet_on();
     dfplayer_reset();
     dfplayer_wait_for_init();
-    dfplayer_set_volume(30);
+    dfplayer_set_volume(20);
 }
 
 /* Shut down DFPlayer*/
